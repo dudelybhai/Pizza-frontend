ed1ae67969ecb602223a7d13f9dd13488c9ab297
dudelybhai <safi@cosmicconsultancy.com>
2021-05-27 19:02:19 +0530

    Docker Update

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,20 +1,21 @@
-const dotenv = require("dotenv");
+const dotenv = require("dotenv")
 
-const config = dotenv.config();
+const config = dotenv.config()
 
 if (config.error) {
-  throw new Error("Could not find .env file");
+	throw new Error("Could not find .env file")
 }
 
-process.env.NODE_ENV = "development" || process.env.NODE_ENV;
+process.env.NODE_ENV = "development" || process.env.NODE_ENV
 
 module.exports = {
-  isDev: process.env.NODE_ENV === "development",
-  port: parseInt(process.env.PORT, 10),
-  db: {
-    host: process.env.DB_HOST,
-    name: process.env.DB_SCHEMA,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
-  }
-};
+	isDev: process.env.NODE_ENV === "development",
+	port: parseInt(process.env.PORT, 10),
+	db: {
+		host: process.env.DB_HOST,
+		username: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_SCHEMA,
+		port: 4321,
+	},
+}
